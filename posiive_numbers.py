bb68fb51176bd6ea83993b4e2c26c13c2ea8b1b9
Praveen <95998093+praveen14402@users.noreply.github.com>
2022-05-22 10:53:13 +0530

    Create posiive_numbers.py

---
+++ posiive_numbers.py
@@ -0,0 +1,9 @@
+start = int (input("Enter start of range : "))
+end =int(input("Enetr end of range : "))
+
+#itterating each number in list
+for num in range (start , end +1):
+
+
+    if num >=0 :
+        print(num , end =" ")
